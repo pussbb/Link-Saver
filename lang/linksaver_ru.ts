bbecb4ac804ff79aaa2934dfeda2f32ff4d99a63
pussbb <pussbb@gmail.com>
2010-10-09 18:46:27 +0300

    added multilang support

---
+++ lang/linksaver_ru.ts
@@ -0,0 +1,234 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../about.ui" line="17"/>
+        <source>About</source>
+        <translation>О програме</translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="30"/>
+        <source>HDA</source>
+        <translation>QLinkSaver</translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="42"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;DejaVu Sans&apos;; font-size:8pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;table border=&quot;0&quot; style=&quot;-qt-table-type: root; margin-top:4px; margin-bottom:4px; margin-left:4px; margin-right:4px;&quot;&gt;
+&lt;tr&gt;
+&lt;td style=&quot;border: none;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;img src=&quot;:/res/2globe_128.png&quot; style=&quot;float: left;&quot; /&gt;&lt;span style=&quot; font-size:14pt; font-weight:600;&quot;&gt;Link Saver 2.0 &lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;justify&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:10pt;&quot;&gt;version 2.0.0.5 alpha.(build 30.03.2010).&lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;justify&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:10pt;&quot;&gt;Licence GNU LGPL v 2.0&lt;/span&gt;&lt;/p&gt;
+&lt;p align=&quot;justify&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:10pt;&quot;&gt;Author :_pussbb.&lt;/span&gt;&lt;span style=&quot; font-size:14pt;&quot;&gt; &lt;/span&gt;&lt;a href=&quot;http://krabik.co.ua/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;Site&lt;/span&gt;&lt;/a&gt;&lt;span style=&quot; font-size:14pt;&quot;&gt; &lt;/span&gt;&lt;span style=&quot; font-size:10pt;&quot;&gt;E-mail: pussbb@gmail.com  &lt;/span&gt;&lt;/p&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="58"/>
+        <source>3rdparty</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="70"/>
+        <source>***********************************************************************
+ PictureFlow - animated image show
+  http://pictureflow.googlecode.com
+  Copyright (C) 2007 Ariya Hidayat (ariya@kde.org)
+  Copyright (C) 2008 Ariya Hidayat (ariya@kde.org)
+********************************************************************
+WebSnap
+Copyright (C) 2009 Nokia Corporation and/or its subsidiary(-ies).
+Contact: Qt Software Information (qt-info@nokia.com)
+
+WebSnap is part of the Graphics Dojo project on Qt Labs.
+***********************************************************************</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="87"/>
+        <source>License</source>
+        <translation>Лицензия</translation>
+    </message>
+    <message>
+        <location filename="../about.ui" line="99"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;DejaVu Sans&apos;; font-size:8pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;table style=&quot;-qt-table-type: root; margin-top:4px; margin-bottom:4px; margin-left:4px; margin-right:4px;&quot;&gt;
+&lt;tr&gt;
+&lt;td style=&quot;border: none;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;/p&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>AddUrl</name>
+    <message>
+        <location filename="../addurl.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Диалог</translation>
+    </message>
+    <message>
+        <location filename="../addurl.ui" line="78"/>
+        <source>Category</source>
+        <translation>Категория</translation>
+    </message>
+</context>
+<context>
+    <name>Apps</name>
+    <message>
+        <location filename="../apps.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Диалог</translation>
+    </message>
+    <message>
+        <location filename="../apps.ui" line="26"/>
+        <source>Application</source>
+        <translation>Приложение</translation>
+    </message>
+    <message>
+        <location filename="../apps.ui" line="38"/>
+        <source>Displayed Name</source>
+        <translation>Имя</translation>
+    </message>
+    <message>
+        <location filename="../apps.ui" line="71"/>
+        <source>Executable</source>
+        <translation>Exe файл</translation>
+    </message>
+    <message>
+        <location filename="../apps.ui" line="84"/>
+        <source>Icon</source>
+        <translation>Иконка</translation>
+    </message>
+    <message>
+        <location filename="../apps.ui" line="107"/>
+        <source>Screenshot</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../apps.ui" line="130"/>
+        <location filename="../apps.ui" line="143"/>
+        <location filename="../apps.ui" line="156"/>
+        <source>...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../apps.cpp" line="22"/>
+        <location filename="../apps.cpp" line="28"/>
+        <location filename="../apps.cpp" line="34"/>
+        <source>Open Image</source>
+        <translation>Открыть изображение</translation>
+    </message>
+    <message>
+        <location filename="../apps.cpp" line="22"/>
+        <source>Executable(*.*)</source>
+        <translation>Выбрать исполняемый файл</translation>
+    </message>
+    <message>
+        <location filename="../apps.cpp" line="28"/>
+        <location filename="../apps.cpp" line="34"/>
+        <source>Image Files (*.png *.jpg *.svg)</source>
+        <translation>Выбрать Картинку</translation>
+    </message>
+</context>
+<context>
+    <name>LinkSaver</name>
+    <message>
+        <location filename="../linksaver.ui" line="14"/>
+        <source>LinkSaver</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../linksaver.ui" line="65"/>
+        <source>Links</source>
+        <translation>Ссылки</translation>
+    </message>
+    <message>
+        <location filename="../linksaver.ui" line="83"/>
+        <source>Help</source>
+        <translation>Помощь</translation>
+    </message>
+    <message>
+        <location filename="../linksaver.ui" line="90"/>
+        <source>File</source>
+        <translation>Файл</translation>
+    </message>
+    <message>
+        <location filename="../linksaver.ui" line="106"/>
+        <location filename="../linksaver.cpp" line="308"/>
+        <source>Add Url</source>
+        <translation>Добавить ссылку</translation>
+    </message>
+    <message>
+        <location filename="../linksaver.ui" line="111"/>
+        <source>Exit</source>
+        <translation>Выход</translation>
+    </message>
+    <message>
+        <location filename="../linksaver.ui" line="120"/>
+        <location filename="../linksaver.cpp" line="253"/>
+        <source>Add Category</source>
+        <translation>Добавить категорию</translation>
+    </message>
+    <message>
+        <location filename="../linksaver.ui" line="129"/>
+        <source>About</source>
+        <translation>О ...</translation>
+    </message>
+    <message>
+        <location filename="../linksaver.ui" line="134"/>
+        <source>About QT</source>
+        <translation>О QT</translation>
+    </message>
+    <message>
+        <location filename="../linksaver.ui" line="139"/>
+        <source>Remove</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <location filename="../linksaver.ui" line="144"/>
+        <source>Add App</source>
+        <translation>Добавить Приложение</translation>
+    </message>
+    <message>
+        <location filename="../linksaver.cpp" line="41"/>
+        <location filename="../linksaver.cpp" line="55"/>
+        <location filename="../linksaver.cpp" line="69"/>
+        <location filename="../linksaver.cpp" line="83"/>
+        <source>Error creating directory:&lt;br /&gt;</source>
+        <translation>Ошыбка при создании папки&lt;br /&gt;</translation>
+    </message>
+    <message>
+        <location filename="../linksaver.cpp" line="205"/>
+        <source>The program will keep running
+ in the system tray. To terminate the program, 
+ choose Quit in the context menu  
+ of the system tray entry.</source>
+        <translation>Программа продолжить работать 
+в системном трее. Для завершения 
+программы выберете Выход в контексном меню.</translation>
+    </message>
+    <message>
+        <location filename="../linksaver.cpp" line="254"/>
+        <source>Name:</source>
+        <translation>Имя:</translation>
+    </message>
+    <message>
+        <location filename="../linksaver.cpp" line="267"/>
+        <source>Cat menu</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../linksaver.cpp" line="309"/>
+        <source>URL:</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
