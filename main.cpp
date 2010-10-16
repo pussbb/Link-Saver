1b4818526ed38beccbba2203dc490547d549f4c0
pussbb <pussbb@gmail.com>
2010-10-16 18:10:26 +0300

    fix lang id identifire

--- main.cpp
+++ main.cpp
@@ -1,20 +1,28 @@
 #include <QtGui/QApplication>
 #include "linksaver.h"
 #include <QTranslator>
+#if !defined(Q_OS_OS2)
 #include <qtsingleapplication/src/qtsingleapplication.h>
-
+#endif
 int main(int argc, char *argv[])
 {
+
+#if !defined(Q_OS_OS2)
     QtSingleApplication a(argc, argv);
-    LinkSaver w;
-    // CreateMutex(0, true, "your_appname_start_once");
-    // if (GetLastError() == ERROR_ALREADY_EXISTS) exit(0);
     if (a.isRunning())
     {      a.activateWindow();
         return 0;
     }
+#else
+    QApplication a(argc, argv);
+#endif
+    LinkSaver w;
     QString locale = QLocale::system().name();
     QTranslator translator;
+    if(locale.length()>2)
+    {
+        locale.resize(2);
+    }
     translator.load(QDir::toNativeSeparators (QApplication::applicationDirPath()+"/lang/" )+"linksaver_"+locale);
     QApplication::installTranslator(&translator);
     QApplication::setQuitOnLastWindowClosed(false);
