bbbab66d9df51c91d61b92896bbc5c9a7427b61c
pussbb <pussbb@gmail.com>
2012-05-20 21:44:42 +0300

    languages init

---
+++ lang/qlinksaver_def.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>Language</name>
+    <message>
+        <location filename="../src/qcorewindow.cpp" line="42"/>
+        <source>English</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LinkSaver</name>
+    <message>
+        <location filename="../src/linksaver.ui" line="13"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QCoreWindow</name>
+    <message>
+        <location filename="../src/qcorewindow.cpp" line="24"/>
+        <location filename="../src/qcorewindow.cpp" line="58"/>
+        <location filename="../src/qcorewindow.cpp" line="74"/>
+        <source>langmenu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qcorewindow.cpp" line="43"/>
+        <source>&amp;%2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
