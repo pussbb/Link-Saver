c26c388218dd7d45d390998cd3a4fc2486caa682
pussbb <pussbb@madeit.(none)>
2012-07-02 16:22:07 +0300

    tr update

--- lang/qlinksaver_ru.ts
+++ lang/qlinksaver_ru.ts
@@ -9,11 +9,46 @@
     </message>
 </context>
 <context>
-    <name>LinkSaver</name>
+    <name>LinkDialog</name>
     <message>
-        <source>MainWindow</source>
+        <source>Add Link</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>http://</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>URL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Width</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Height</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Zoom</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LinkSaver</name>
     <message>
         <source>File</source>
         <translation type="unfinished"></translation>
@@ -26,6 +61,120 @@
         <source>Exit</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>Link Saver</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Links List</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>New</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Link list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Category</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Link</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>delete List</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>deleteCategory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>deleteLink</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add Category</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Delete list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Do you realy what to delete %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>It will be removed whole directory 
+%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Delete category</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Do you realy what to category 
+ %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Delete link</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Do you realy what to delete link 
+ %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NewList</name>
+    <message>
+        <source>New list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Choose Directory</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>QCoreWindow</name>
@@ -35,6 +184,82 @@
     </message>
     <message>
         <source>&amp;%2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Directory write protected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1
+Please choose another directory.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Url</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Must be valid url adrress</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error creating folder.
+%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Deleting list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Could not delete folder and files.
+%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Deleting category</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Could not delete category.
+%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Deleting link</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Could not delete link.
+%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Directory not found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Directory already contains link list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please choose another directory.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Name is empty</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please type your name of list.</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
